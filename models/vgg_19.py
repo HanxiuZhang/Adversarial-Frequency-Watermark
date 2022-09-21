98ace132a69cd48d8f6ddfc62b29ebd2bf0e5331
LEON <13391355527@163.com>
2022-09-21 13:17:25 +0800

    09211317

--- models/vgg_19.py
+++ models/vgg_19.py
@@ -6,7 +6,7 @@ from typing import Union, List, Any, Optional, cast
 from torchvision.models._utils import _ovewrite_named_param
 from torchvision.models.vgg import VGG19_Weights
 
-def VGG_with_trans(VGG):
+class VGG_with_trans(VGG):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = transforms.Resize([224,224])(x)
         x = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(x)
@@ -48,15 +48,16 @@ def vgg19(*, weights: Optional[VGG19_Weights] = None, progress: bool = True, **k
         model.load_state_dict(weights.get_state_dict(progress=progress))
     return model
 
-def vgg19_in():
+def vgg19_in(**kwargs: Any) -> VGG:
     weights = VGG19_Weights.IMAGENET1K_V1
     weights = VGG19_Weights.verify(weights)
     cfgs_cfg = [64, 64, "M", 128, 128, "M", 256, 256, 256, 256, "M", 512, 512, 512, 512, "M", 512, 512, 512, 512, "M"]
-    model = VGG_with_trans(make_layers(cfgs_cfg, batch_norm=False))
+    model = VGG_with_trans(features=make_layers(cfgs_cfg, batch_norm=False),**kwargs)
     model.load_state_dict(weights.get_state_dict(progress=True))
     return model
 
-def vgg19_cifar():
+def vgg19_cifar(**kwargs: Any) -> VGG:
     cfgs_cfg = [64, 64, "M", 128, 128, "M", 256, 256, 256, 256, "M", 512, 512, 512, 512, "M", 512, 512, 512, 512, "M"]
-    model = VGG_with_trans(make_layers(cfgs_cfg, batch_norm=False), num_classes=10)
+    # model = VGG_with_trans(features=make_layers(cfgs_cfg, batch_norm=False), num_classes=10,**kwargs)
+    model = VGG_with_trans(features=make_layers(cfgs_cfg, batch_norm=False), num_classes=10,**kwargs)
     return model
