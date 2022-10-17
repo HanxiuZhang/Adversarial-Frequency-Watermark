e114e039ca200dc9b17dcd3da6a9f446e9c9fa21
HanxiuZhang <hanxiu.zhang@hotmail.com>
2022-10-17 13:13:36 +0800

    10171313backup

--- attacks/pgd_l2.py
+++ attacks/pgd_l2.py
@@ -9,29 +9,41 @@ from utils import *
 sys.path.append('../attacks/')
 from opti import *
 
-def ifgsm_direct(img: Tensor, label: Tensor, wm: Tensor, model: nn.Module, alpha:float, beta: float,block_size: int=8, steps: int=10, eps: float=10/255) -> Tensor:
+def pgdl2_direct(img: Tensor, label: Tensor, wm: Tensor, model: nn.Module, alpha:float, beta: float, block_size: int=8, steps: int=10, eps: float=10/255,eps_for_division: float=10/255) -> Tensor:
     r'''
     Pipline method to 1) embed digital watermark 2) add perturbation in iterative-FGSM way
     '''
     # Embed watermark
     wmed_img = embed_wm(img,wm,alpha,block_size)
     # Calculate perturbation
-    adv_img = wmed_img.clone()
+    adv_img = wmed_img.clone().detach().unsqueeze(0)
+    images = adv_img.clone().detach()
     loss = nn.CrossEntropyLoss()
-    adv_img = adv_img.unsqueeze(0)
-    adv_img.requires_grad = True
-    for _ in range(steps):
-        outputs = model(adv_img)
-        cost = loss(outputs,label)
-        grad = torch.autograd.grad(cost,adv_img,retain_graph=False,create_graph=False)[0]
-        adv_img = adv_img + beta*grad.sign()
-        adv_img = torch.clamp(adv_img,min=0,max=1)
-    # Clip perturbation to an range to avoid image distortion
-    delta = (adv_img - wmed_img).clip(-1*eps,eps).squeeze(0)
-    res = (wmed_img + delta).clip(0,1)
-    return res
 
-def ifgsm_wm_opti(img: Tensor, label: Tensor, wm: Tensor, model: nn.Module, 
+    for _ in range(steps):
+        adv_img.requires_grad = True
+        outputs = model(adv_img)
+
+        # Calculate loss
+        cost = loss(outputs, label)
+
+        # Update adversarial images
+        grad = torch.autograd.grad(cost, adv_img,
+                                    retain_graph=False, create_graph=False)[0]
+        grad_norms = torch.norm(grad) + eps_for_division
+        grad = grad / grad_norms.view(1, 1, 1, 1)
+        adv_img = adv_img.detach() + beta * grad
+
+        delta = adv_img - images
+        delta_norms = torch.norm(delta.view(1, -1), p=2, dim=1)
+        factor = eps / delta_norms
+        factor = torch.min(factor, torch.ones_like(delta_norms))
+        delta = delta * factor.view(-1, 1, 1, 1)
+
+        adv_img = torch.clamp(images + delta, min=0, max=1).detach()
+    return adv_img.squeeze(0)
+
+def pgdl2_wm_opti(img: Tensor, label: Tensor, wm: Tensor, model: nn.Module, 
                 alpha:float, beta: float,block_size: int, steps: int, eps:float,
                 N: int, l1: float, l2: float, s_a: float, s_b: float, beta_max: float) -> Tensor:
     r'''
